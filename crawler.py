24fcabc86cf2526ef68790adf323b6b38d6215cd
Arnav Bhate <bhatearnav@gmail.com>
2024-04-10 15:15:38 +0530

    Improvements

--- crawler.py
+++ crawler.py
@@ -2,6 +2,8 @@ import requests
 from html.parser import HTMLParser
 from urllib.parse import urljoin
 
+website = "https://krittikaiitb.github.io/"
+
 
 class Parser(HTMLParser):
     def __init__(self):
@@ -9,14 +11,34 @@ class Parser(HTMLParser):
         self.links = []
 
     def handle_starttag(self, tag, attrs):
-        if tag in ["a", "area", "link", "base"]:
+        if tag in ["a", "area", "link", "base", "iframe"]:
             for attr in attrs:
-                if attr[0] == "href":
-                    self.links.append(attr[1])
+                if attr[0] in ["href", "src"]:
+                    url = urljoin(website, attr[1])
+                    # Check for local domain and some common file extensions
+                    if url.startswith(website) and url.split(".")[-1] not in [
+                        "css",
+                        "js",
+                        "pdf",
+                        "txt",
+                        "csv",
+                        "jpg",
+                        "jpeg",
+                        "png",
+                        "gif",
+                        "svg",
+                        "ico",
+                        "mp4",
+                        "mp3",
+                    ]:
+                        link = [url, True]
+                    else:
+                        link = [url, False]
+                    if link not in self.links:
+                        self.links.append(link)
         elif tag in [
             "audio",
             "embed",
-            "iframe",
             "img",
             "input",
             "script",
@@ -26,7 +48,9 @@ class Parser(HTMLParser):
         ]:
             for attr in attrs:
                 if attr[0] == "src":
-                    self.links.append(attr[1])
+                    link = [urljoin(website, attr[1]), False]
+                    if link not in self.links:
+                        self.links.append(link)
 
 
 def get_links(url: str, links: list) -> None:
@@ -36,29 +60,33 @@ def get_links(url: str, links: list) -> None:
     response = requests.get(url)
     content = response.text
 
+    # Check if the content is HTML
+    if "html" not in response.headers["Content-Type"]:
+        return
+
     # Parse the content
     parser = Parser()
     parser.feed(content)
 
     # Get links
     for link in parser.links:
-        if link.startswith("http"):
-            resolved_link = link
+        for l in links:
+            if link[0] == l[0]:
+                break
         else:
-            resolved_link = urljoin(url, link)
-        if resolved_link not in links:
-            links.append(resolved_link)
+            links.append(link)
     final_length = len(links)
+    parser.close()
 
     # Recursively get links (local only)
     for link in links[initial_length:final_length]:
-        if link.startswith("https://krittikaiitb.github.io/"):
-            get_links(link, links)
+        if link[1]:
+            get_links(link[0], links)
 
 
 if __name__ == "__main__":
-    links = []
-    get_links("https://krittikaiitb.github.io/", links)
+    links = [[website, "True"]]
+    get_links(links[0][0], links)
     print("Links found:")
     for link in links:
-        print(link)
+        print(link[0])
